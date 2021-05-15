879da645643c7f3bee65e24a4daeb140d469ac74
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-05-15 07:20:49 +0000

    テスト実行確認

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "testMatch": [
+        "**/__tests__/**/*.+(t|j)s"
+    ],
+    "transform": {
+        "^.+\\.(ts|tsx)$": "ts-jest"
+    }
+}
---
+++ src/__tests__/jest.ts
@@ -0,0 +1,5 @@
+describe('jest',()=>{
+    it('jest test',()=>{
+        expect(1).toBe(1);
+    })
+})
